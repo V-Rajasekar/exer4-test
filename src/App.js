826ad96bdc5b5f973bc53b032fb342302e6bc1dc
Rajaseka <rajasekar.vadivelu@gmail.com>
2023-05-12 08:21:42 +0200

    Add CNAME and InvestIndex page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import logo from './me.jpg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( <
+        div className = "App" >
+        <
+        header className = "App-header" > { < img src = { logo }
+            className = "App-logo"
+            alt = "logo" / >
+        } < p >
+        Rajasekar < /p> <
+        a className = "App-link"
+        href = "./InvestIndex.html"
+        target = "_blank"
+        rel = "noopener noreferrer" >
+        Invest Index < /a> </header >
+
+        <
+        /div>
+    );
 }
 
 export default App;
